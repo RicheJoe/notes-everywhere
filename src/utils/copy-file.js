84728124d9d0054ef3b46c5ba80bceadcc015330
zhouhang <zhouhang@dataexa.com>
2022-01-12 10:49:39 +0800

    description

--- src/utils/copy-file.js
+++ src/utils/copy-file.js
@@ -1,3 +1,5 @@
+//这是一个复制文件带相应目录得脚本
+//使用方法  修改代码内文件路径和后缀
 const path = require("path");
 const fs = require("fs");
 const copyFile = (sourcePath, targetPath) => {
@@ -11,14 +13,16 @@ const copyFile = (sourcePath, targetPath) => {
       // isExist(newTargetPath);
       copyFile(newSourcePath, newTargetPath);
     }
-    if (file.name.includes("_")) {
-      fs.unlink(newSourcePath, err => {
-        if (err) {
-          console.log(err);
-        }
-        console.log("文件:" + newSourcePath + "删除成功！");
-      });
-    }
+    //删除文件名带_的文件
+    // if (file.name.includes("_")) {
+    //   fs.unlink(newSourcePath, err => {
+    //     if (err) {
+    //       console.log(err);
+    //     }
+    //     console.log("文件:" + newSourcePath + "删除成功！");
+    //   });
+    // }
+    //复制文件  rename会移动文件
     if (file.name.endsWith(".pdf")) {
       console.log(file.name);
       let newPath = "D:/Project/webpack_demo/output/pdf/" + file.name;
@@ -28,6 +32,7 @@ const copyFile = (sourcePath, targetPath) => {
         }
       });
       // console.log(file.name, newSourcePath, newTargetPath);
+      // 这个会复制文件  但是必须有相应的目录
       // fs.copyFileSync(newSourcePath, newTargetPath);
     }
   });
